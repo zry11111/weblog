4a68737b71968a053980ffd939a39833fd479741
ren <2336252728@qq.com>
2025-06-03 21:06:56 +0800

    修改编译方式

--- weblog-web/src/main/java/com/zry/weblog/web/WeblogWebApplication.java
+++ weblog-web/src/main/java/com/zry/weblog/web/WeblogWebApplication.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @ComponentScan({"com.zry.weblog.*"})
 public class WeblogWebApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WeblogWebApplication.class, args);
     }
